7e6a3c228e99b1da13359260e320d212faa55bcf
Jayceon <jayceon310893@gmail.com>
2022-10-12 13:17:04 +0300

    vue-test splitChunks

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,1 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
 
-  css: {
-      extract: false,
-  },
-configureWebpack: {
-    optimization: {
-        splitChunks: false,
-    }
-}
-})
